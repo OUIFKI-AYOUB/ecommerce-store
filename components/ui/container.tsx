b91478c4eae3d91daca701e4cd0b56ace27d7372
OUIFKI-AYOUB <150385854+OUIFKI-AYOUB@users.noreply.github.com>
2025-02-13 06:37:50 +0100

    Update container.tsx

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -1,14 +1,15 @@
 interface ContainerProps {
-    children : React.ReactNode;
+    children: React.ReactNode;
 }
-const Container:React.FC<ContainerProps> = ({
+
+const Container: React.FC<ContainerProps> = ({
     children
 }) => {
-  return (
-    <div className="mx-auto max-w-7xl ">
-        {children}
-     </div>
-  )
+    return (
+        <div className="mx-auto max-w-7xl zoom-enabled">
+            {children}
+        </div>
+    )
 }
 
 export default Container
